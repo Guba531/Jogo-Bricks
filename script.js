f993428f8220acf58d32753bbdd2658ff5d7ac87
Guba <gustavopessoa211@gmail.com>
2025-01-22 17:10:21 -0800

    Funções de desenho e movimento do paddle

--- script.js
+++ script.js
@@ -6,3 +6,42 @@ const ctx = canvas.getContext('2d');
 
 // Exibimos uma mensagem no console para confirmar que o jogo está configurado corretamente
 console.log("O jogo está configurado e pronto para iniciar!");
+
+//Variaveis para controlar a posição e o movimento do paddle
+let paddle = 350;
+const paddleWidth = 100;
+const paddleHeight = 10;
+const paddleSpeed = 7;
+let isMovingLeft = false;
+let isMovingRight = false;
+
+//Função para desenhar o paddle 
+function drawPaddle() {
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(350, 550, 100, 10);
+ }
+
+//Função para mover o paddle
+function movePaddle() {
+  if (isMovingLeft && paddleX > 0) {
+    paddleX -= paddleSpeed;
+  }
+  if (isMovingRight && paddleX < canvas.width - paddleWidth) {
+    paddleX += paddleSpeed;
+  }
+ }
+
+//Função para limpar o canvas antes de redesenhar
+function clearCanvas() {
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+//Função principal que atualiza a tela
+function update() {
+clearCanvas();
+drawPaddle();
+requestAnimationFrame(update);
+}
+
+//Inicia o loop de atualização
+update();
